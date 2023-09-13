179ba36a4221dc64753baea18d6e5f2bc04b052b
trillo <sk8unk@gmail.com>
2023-09-12 21:11:04 -0300

    touches

--- config/app.php
+++ config/app.php
@@ -28,7 +28,7 @@ return [
     |
     */
 
-    'env' => env('APP_ENV', 'development'),
+    'env' => env('APP_ENV', 'production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -41,7 +41,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', true),
+    'debug' => (bool) env('APP_DEBUG', false),
 
     /*
     |--------------------------------------------------------------------------
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    App::setLocale('es-CL');
     return view('welcome');
 });
